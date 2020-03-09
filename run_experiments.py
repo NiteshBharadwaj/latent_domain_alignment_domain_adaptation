72e8a0fee747c83ae7957b01df44072fc2d972e9
Taruj Goyal <taruj95@gmail.com>
2020-03-08 17:20:06 -0700

    Minor update

--- run_experiments.py
+++ run_experiments.py
@@ -1,4 +1,5 @@
 import os
+from tqdm import tqdm 
 
 dataset = ['mnistm', 'mnist', 'usps', 'svhn', 'syn']
 epochs = 100
@@ -6,20 +7,20 @@ gpuid = 0
 experiments = ['hard_cluster','soft_cluster']
 k = 4
 
-for data in dataset:
-    for exp in experiments:
-        for i in range(5):
+for data in tqdm(dataset):
+    for exp in tqdm(experiments):
+        for i in tqdm(range(5)):
             command = 'bash experiment_do.sh %s %d %d record/%s_%s_clus_%d %s digits %d' % (data,epochs,gpuid,data,exp,k,exp,k)
             print(command)
-            #os.system(command)
+            os.system(command)
             #bash experiment_do.sh  usps 100 0 record/usps_MSDA_{exp_type} digits 4
 
 k_list = [1,2,5,8,10]
 
-for data in dataset:
-    for exp in experiments:
-        for k in k_list:
+for data in tqdm(dataset):
+    for exp in tqdm(experiments):
+        for k in tqdm(k_list):
             command = 'bash experiment_do.sh %s %d %d record/%s_%s_clus_%d %s digits %d' % (data,epochs,gpuid,data,exp,k,exp,k)
             print(command)
-            #os.system(command)
+            os.system(command)
 
