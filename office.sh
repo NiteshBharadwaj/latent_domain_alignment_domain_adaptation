8691a85aa09331f864fde8b8222953012fa65220
Taruj Goyal <taruj95@gmail.com>
2020-04-29 19:27:00 -0700

    Files to run comprehensive experiments

--- office.sh
+++ office.sh
@@ -1,47 +1,45 @@
-# bash ./experiment_do.sh webcam 500 0 record/amazon_webcam_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh webcam 400 0 record/amazon_webcam_source_only source_only office 6 no
-# bash ./experiment_do.sh webcam 200 0 record/amazon_webcam_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh amazon 500 0 record/webcam_amazon_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh amazon 400 0 record/webcam_amazon_source_only source_only office 6 no
-# bash ./experiment_do.sh amazon 200 0 record/webcam_amazon_source_target source_target_only office 12 no
-
-
+bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh daw 1500 0 record_office/dslr_amazon_webcam_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh dwa 1500 0 record_office/dslr_webcam_amazon_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh wad 1500 0 record_office/webcam_amazon_dslr_soft_cluster_4 soft_cluster office 4 no
+bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh daw 1500 0 record_office/dslr_amazon_webcam_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh dwa 1500 0 record_office/dslr_webcam_amazon_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh wad 1500 0 record_office/webcam_amazon_dslr_soft_cluster_12 soft_cluster office 12 no
+bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh daw 1500 0 record_office/dslr_amazon_webcam_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh dwa 1500 0 record_office/dslr_webcam_amazon_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh wad 1500 0 record_office/webcam_amazon_dslr_soft_cluster_2 soft_cluster office 2 no
+bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh daw 1500 0 record_office/dslr_amazon_webcam_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh dwa 1500 0 record_office/dslr_webcam_amazon_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh wad 1500 0 record_office/webcam_amazon_dslr_soft_cluster_8 soft_cluster office 8 no
+bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_6 soft_cluster office 6 no
+bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_6 soft_cluster office 6 no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_6 soft_cluster office 6 no
 bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_soft_cluster_6 soft_cluster office 6 no
-bash ./experiment_do.sh dw 200 0 record_office/dslr_webcam_source_only source_only office 6 no
-bash ./experiment_do.sh dw 1500 0 record_office/dslr_webcam_source_target source_target_only office 12 no
-
+bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_6 soft_cluster office 6 no
 bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_soft_cluster_6 soft_cluster office 6 no
-bash ./experiment_do.sh wd 200 0 record_office/webcam_dslr_source_only source_only office 6 no
-bash ./experiment_do.sh wd 1500 0 record_office/webcam_dslr_source_target source_target_only office 12 no
-
-
-bash ./experiment_do.sh adw 1500 0 record_office/amazon_dslr_webcam_soft_cluster_6 soft_cluster office 6 no
-bash ./experiment_do.sh adw 200 0 record_office/amazon_dslr_webcam_source_only source_only office 6 no
-bash ./experiment_do.sh adw 1500 0 record_office/amazon_dslr_webcam_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh da 200 0 record_office/dslr_amazon_source_only source_only office 6 no
-# bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh aw 200 0 record_office/amazon_webcam_source_only source_only office 6 no
-# bash ./experiment_do.sh aw 1500 0 record_office/amazon_webcam_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh wa 200 0 record_office/webcam_amazon_source_only source_only office 6 no
-# bash ./experiment_do.sh wa 1500 0 record_office/webcam_amazon_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh ad 200 0 record_office/amazon_dslr_source_only source_only office 6 no
-#bash ./experiment_do.sh ad 1500 0 record_office/amazon_dslr_source_target source_target_only office 2 no
-
-# bash ./experiment_do.sh awd 1500 0 record_office/amazon_webcam_dslr_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh awd 200 0 record_office/amazon_webcam_dslr_source_only source_only office 6 no
-# bash ./experiment_do.sh awd 1500 0 record_office/amazon_webcam_dslr_source_target source_target_only office 12 no
-
-# bash ./experiment_do.sh wda 1500 0 record_office/webcam_dslr_amazon_soft_cluster_6 soft_cluster office 6 no
-# bash ./experiment_do.sh wda 200 0 record_office/webcam_dslr_amazon_source_only source_only office 6 no
-# bash ./experiment_do.sh wda 1500 0 record_office/webcam_dslr_amazon_source_target source_target_only office 12 no
-
-
+bash ./experiment_do.sh daw 1500 0 record_office/dslr_amazon_webcam_soft_cluster_6 soft_cluster office 6 no
+bash ./experiment_do.sh dwa 1500 0 record_office/dslr_webcam_amazon_soft_cluster_6 soft_cluster office 6 no
+bash ./experiment_do.sh wad 1500 0 record_office/webcam_amazon_dslr_soft_cluster_6 soft_cluster office 6 no
--- run_experiments.py
+++ run_experiments.py
@@ -1,20 +1,44 @@
-import os
-from tqdm import tqdm 
+#import os
+#from tqdm import tqdm 
+#
+#dataset = ['mnistm', 'mnist', 'usps', 'svhn', 'syn']
+#
+#epochs = 100
+#gpuid = 0
+#experiments = ['soft_cluster','hard_cluster']
+#
+#k_list = [4,5,6,8,10]
+#
+#for k in tqdm(k_list):
+#    for exp in tqdm(experiments):
+#        for data in tqdm(dataset):
+#            command = 'bash experiment_do.sh %s %d %d record/%s_%s_clus_%d %s digits %d yes' % (data,epochs,gpuid,data,exp,k,exp,k)
+#            print(command)
+#            os.system(command)
+#            # bash experiment_do.sh mnistm 100 0 record/mnistm_baseline soft_cluster digits 4 yes
 
-dataset = ['mnistm', 'mnist', 'usps', 'svhn', 'syn']
 
-epochs = 100
 gpuid = 0
-experiments = ['soft_cluster','hard_cluster']
-#,'source_only']
+experiments1 = ['source_only','source_target']
 
-k_list = [4]
-#k_list = [2,4,5,6,8,10]
+experiments = ['soft_cluster']
 
-for k in tqdm(k_list):
-    for exp in tqdm(experiments):
-        for data in tqdm(dataset):
-            command = 'bash experiment_do.sh %s %d %d record/%s_%s_clus_%d %s digits %d yes' % (data,epochs,gpuid,data,exp,k,exp,k)
-            print(command)
-            os.system(command)
-            # bash experiment_do.sh mnistm 100 0 record/mnistm_baseline soft_cluster digits 4 yes
+
+epochs = {'source_only': 500,'source_target':1500,'soft_cluster':1500}
+
+datasets = {'ad': 'amazon_dslr', 'aw': 'amazon_webcam', 'da': 'dslr_amazon', 'dw': 'dslr_webcam', 'wa': 'webcam_amazon', 'wd': 'webcam_dslr','daw': 'dslr_amazon_webcam', 'dwa': 'dslr_webcam_amazon', 'wad': 'webcam_amazon_dslr'}
+
+k_list = [4,12,2,8,6]
+
+exp_list = []
+
+for k in k_list:
+  for data in datasets:
+    for exp in experiments:
+      command = 'bash ./experiment_do.sh %s %d %d record_office/%s_%s_%d %s office %d no' % (data,epochs[exp],gpuid,datasets[data],exp,k,exp,k)
+      #print(command)
+      exp_list.append(command)
+      #command = 'bash ./experiment_do.sh %s %d %d record_office/%s_%s %s office %d no' % (data,epochs[exp],gpuid,datasets[data],exp,exp,k)
+
+for a in exp_list:
+  print(a)
