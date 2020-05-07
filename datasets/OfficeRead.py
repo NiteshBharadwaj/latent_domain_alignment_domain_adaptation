57e0bd4f7a6a192735c9b297ab49abda56954ef0
NiteshBharadwaj <ntesh93@gmail.com>
2020-05-07 06:55:29 +0000

    changes

--- datasets/OfficeRead.py
+++ datasets/OfficeRead.py
@@ -1,8 +1,8 @@
 import csv
 def read_office_domain(domain, office_directory):
-	trainFile = office_directory + domain + '_data_train.csv'
-	testFile = office_directory + domain + '_data_train.csv'
-	validFile = office_directory + domain + '_data_train.csv'
+	trainFile = office_directory+'/data/office_sample/' + domain + '_data_train.csv'
+	testFile = office_directory+'/data/office_sample/' + domain + '_data_train.csv'
+	validFile = office_directory+'/data/office_sample/' + domain + '_data_train.csv'
 
 	trainReader = csv.reader(open(trainFile, 'r'))
 	testReader = csv.reader(open(testFile, 'r'))
@@ -11,15 +11,15 @@ def read_office_domain(domain, office_directory):
 	paths_train, labels_train, paths_test, labels_test, paths_valid, labels_valid = [], [], [], [], [], []
 
 	for row in trainReader:
-		paths_train.append('.'+row[2][1:])
+		paths_train.append(office_directory+row[2][1:])
 		labels_train.append(int(row[1]))
 
 	for row in testReader:
-		paths_test.append('.'+row[2][1:])
+		paths_test.append(office_directory+row[2][1:])
 		labels_test.append(int(row[1]))
 
 	for row in validReader:
-		paths_valid.append('.'+row[2][1:])
+		paths_valid.append(office_directory+row[2][1:])
 		labels_valid.append(int(row[1]))
 
 	return paths_train, labels_train, paths_test, labels_test, paths_valid, labels_valid
--- office.sh
+++ office.sh
@@ -1,2 +1,2 @@
-bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_8_1 soft_cluster office 8 0.02 1 ./data/office_sample/ no
-bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_8_1 soft_cluster office 8 0.02 1 ./data/office_sample/ yes
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_8_1 soft_cluster office 8 0.02 1 . no
+bash ./experiment_do.sh da 1500 0 record_office/dslr_amazon_soft_cluster_8_1 soft_cluster office 8 0.02 1 . yes
