267b1414ab010c87a854f603c799c088594eceed
root <tgoyal@ucsd.edu>
2020-09-09 00:36:56 +0000

    Dataloader Fix

--- datasets/PACS_read.py
+++ datasets/PACS_read.py
@@ -15,15 +15,15 @@ def read_pacs_domain(domain, pacs_directory, is_target, seed_id):
     paths_train, labels_train, paths_test, labels_test, paths_valid, labels_valid = [], [], [], [], [], []
 
     for row in trainReader:
-        paths_train.append(pacs_directory + row[2][1:])
-        labels_train.append(int(row[1]))
+        paths_train.append(pacs_directory + row[0].split()[0])
+        labels_train.append(int(row[0].split()[1]))
 
     for row in testReader:
-        paths_test.append(pacs_directory + row[2][1:])
-        labels_test.append(int(row[1]))
+        paths_test.append(pacs_directory + row[0].split()[0])
+        labels_test.append(int(row[0].split()[1]))
 
     for row in validReader:
-        paths_valid.append(pacs_directory + row[2][1:])
-        labels_valid.append(int(row[1]))
+        paths_valid.append(pacs_directory + row[0].split()[0])
+        labels_valid.append(int(row[0].split()[1]))
 
     return paths_train, labels_train, paths_test, labels_test, paths_valid, labels_valid
