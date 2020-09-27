0dd2a7894d86e6053bf990a3ac836bf9754d0164
NiteshBharadwaj <ntesh93@gmail.com>
2020-09-27 12:38:13 -0700

    pacs - dl changes

--- datasets/PACS_read.py
+++ datasets/PACS_read.py
@@ -2,6 +2,8 @@ import csv
 import random
 import numpy as np
 
+num_valid_per_class = 5
+
 def read_pacs_domain(domain, pacs_directory, is_target, seed_id):
     random.seed(seed_id)
     trainFile = pacs_directory + '/splits/' + domain + '_train_kfold.txt'
@@ -18,12 +20,34 @@ def read_pacs_domain(domain, pacs_directory, is_target, seed_id):
         paths_train.append(pacs_directory + row[0].split()[0])
         labels_train.append(int(row[0].split()[1])-1)
 
-    for row in testReader:
-        paths_test.append(pacs_directory + row[0].split()[0])
-        labels_test.append(int(row[0].split()[1])-1)
+    if not is_target:
+        for row in testReader:
+            paths_test.append(pacs_directory + row[0].split()[0])
+            labels_test.append(int(row[0].split()[1])-1)
 
-    for row in validReader:
-        paths_valid.append(pacs_directory + row[0].split()[0])
-        labels_valid.append(int(row[0].split()[1])-1)
+        for row in validReader:
+            paths_valid.append(pacs_directory + row[0].split()[0])
+            labels_valid.append(int(row[0].split()[1])-1)
+    else:
+        classwise_imgs = {}
+
+        for row in validReader:
+            if int(row[1]) in classwise_imgs:
+                classwise_imgs[int(row[0].split()[1])-1].append(pacs_directory + row[0].split()[0])
+            else:
+                classwise_imgs[int(row[0].split()[1])-1] = [pacs_directory + row[0].split()[0]]
+
+        for label_idx in classwise_imgs:
+            tmp_arr = list(np.arange(len(classwise_imgs[label_idx])))
+            sampled_arr = random.sample(tmp_arr, num_valid_per_class)
+            for sampled_idx in sampled_arr:
+                paths_valid.append(classwise_imgs[label_idx][sampled_idx])
+                labels_valid.append(label_idx)
+
+        for row in testReader:
+            img_name = pacs_directory + row[0].split()[0]
+            if not (img_name in paths_valid):
+                paths_test.append(img_name)
+                labels_test.append(int(row[0].split()[1])-1)
 
     return paths_train, labels_train, paths_test, labels_test, paths_valid, labels_valid
