cf47a2c3b1d4bf6a464de80c93f823ed9dd319d7
NiteshBharadwaj <ntesh93@gmail.com>
2021-02-24 00:38:47 -0800

    Filter targets

--- metric/class_domain_da.py
+++ metric/class_domain_da.py
@@ -39,8 +39,16 @@ def moment_soft(output_s, output_t, class_prob_s, class_prob_t, domain_prob_s, l
     domain_prob_sum_s = domain_prob_sum_s.reshape(1, c, d) # SHAPE -> 1 x c x d
 
     output_prob_s = (output_prob_s.sum(0)/domain_prob_sum_s) #SHAPE -> e x c x d
-
     output_t = output_t.reshape(output_t.shape[0], output_t.shape[1],1)
+    class_prob_argmax = class_prob_t.argmax(1)
+    class_prob_max = class_prob_t.max(1)
+    confident_targets = class_prob_max>0.8
+    output_t = output_t[confident_targets]
+    class_prob_t = class_prob_t[confident_targets]
+    class_prob_argmax = class_prob_argmax[confident_targets]
+    useful_classes, counts = torch.unique(class_prob_argmax, return_counts=True)
+    useful_classes = useful_classes[counts>=5].detach()
+
     class_prob_t = class_prob_t.reshape(class_prob_t.shape[0], 1, class_prob_t.shape[1])
     output_prob_t = torch.matmul(output_t, class_prob_t) # SHAPE -> N x e x c
     class_prob_sum_t = class_prob_t.sum(0) + 1e-6
@@ -50,14 +58,14 @@ def moment_soft(output_s, output_t, class_prob_s, class_prob_t, domain_prob_s, l
     intra_domain_loss = 0
     inter_domain_loss = 0
     class_tear_apart_loss = 0
-    for cc in range(output_prob_s.shape[1]):
+    for cc in useful_classes:
         for dd in range(output_prob_s.shape[2]):
             inter_domain_loss += domain_prob_sum_s[0,cc,dd]*class_prob_sum_t[0,cc]*euclidean(output_prob_s[:, cc, dd], output_prob_t[:, cc])/(output_s.shape[0]**2)
             for dd2 in range(dd+1, output_prob_s.shape[2]):
                 intra_domain_loss += domain_prob_sum_s[0,cc,dd]*domain_prob_sum_s[0,cc,dd2]*euclidean(output_prob_s[:, cc, dd], output_prob_s[:, cc, dd2])/(output_s.shape[0]**2)
     if moment == 1:
-        for cc1 in range(output_prob_s.shape[1]):
-            for cc2 in range(cc1+1, output_prob_s.shape[1]):
+        for cc1 in useful_classes:
+            for cc2 in useful_classes:
                 for dd in range(output_prob_s.shape[2]):
                     for dd2 in range(dd, output_prob_s.shape[2]):
                         class_tear_apart_loss += domain_prob_sum_s[0,cc1,dd]*domain_prob_sum_s[0,cc2,dd2]*euclidean(output_prob_s[:, cc1, dd], output_prob_s[:, cc2, dd2])/(output_s.shape[0]**2)
